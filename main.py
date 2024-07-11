b8a5b3eb01451c15708aa61cb8b64500a95de3a8
Francisco Samir Pinheiro <34774556+fscpinheiro@users.noreply.github.com>
2024-07-11 01:26:29 -0300

    Checagem de Duplicidade de Nós e de Chaves

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ class Application(tk.Tk):
         self.trans_file_langs = {}
         self.columns = ['ID', 'Chave', 'Valor']
         self.analysis_results_tree = None
+
         self.create_menu()
+
         # Frame para seleção de arquivos base
         self.base_file_frame = tk.Frame(self)
         self.base_file_frame.pack(fill='x')
@@ -56,6 +58,8 @@ class Application(tk.Tk):
         # Botão de análise
         self.analyze_button = tk.Button(self.options_frame, text="Analisar", command=self.analyze_files)
         self.analyze_button.pack(side='left')
+        self.check_duplicates_button = tk.Button(self.options_frame, text="Verificar Duplicatas", command=self.check_duplicates)
+        self.check_duplicates_button.pack(side='left')
         # Abas de relatório
         self.tab_control = ttk.Notebook(self)
         self.tab_control.pack(expand=1, fill='both')
@@ -71,17 +75,30 @@ class Application(tk.Tk):
         self.context_menu.add_command(label="Copiar", command=self.copy_to_clipboard)
         self.context_menu.add_command(label="Traduzir para inglês", command=self.translate_to_english)
         self.context_menu.add_command(label="Traduzir para espanhol", command=self.translate_to_spanish)
+        # Vincula o evento de clique com o botão direito do mouse ao menu de contexto
         self.bind("<Button-3>", self.show_context_menu)
+        # Adiciona uma nova aba para exibir os resultados
+        self.duplicates_tab = ttk.Frame(self.tab_control)
+        self.tab_control.add(self.duplicates_tab, text='Chaves Duplicadas')
+        # Adiciona um novo Treeview para exibir os resultados
+        self.duplicates_results_tree = ttk.Treeview(self.duplicates_tab)
+        self.duplicates_results_tree.pack(expand=1, fill='both')
+        # Maximizar a janela
         self.state('zoomed')
 
+
     def create_menu(self):
+        # Cria uma nova barra de menu
         menu_bar = tk.Menu(self)
+
+        # Cria um menu "Configurações" e adiciona alguns itens
         settings_menu = tk.Menu(menu_bar, tearoff=0)
         settings_menu.add_command(label="Definir idiomas",
-                                  command=self.set_trans_file_langs)  
+                                  command=self.set_trans_file_langs)  # Adiciona um item "Definir idiomas" ao menu "Configurações"
         menu_bar.add_cascade(label="Configurações",
-                             menu=settings_menu)  
+                             menu=settings_menu)  # Adiciona o menu "Configurações" à barra de menu
 
+        # Configura a barra de menu da janela para ser a barra de menu que acabamos de criar
         self.config(menu=menu_bar)
 
     def get_keys(self, dic, parent_key = ''):
@@ -105,10 +122,13 @@ class Application(tk.Tk):
     def check_keys(self, file_path1, file_path2):
         with open(file_path1, 'r', encoding='utf-8') as f:
             data1 = json.load(f)
+
         with open(file_path2, 'r', encoding='utf-8') as f:
             data2 = json.load(f)
+
         keys1 = self.get_keys(data1)
         keys2 = self.get_keys(data2)
+
         result = []
         for key in keys1:
             value = self.get_value(data1, key)
@@ -119,6 +139,7 @@ class Application(tk.Tk):
                 'PT': value,
                 file_path2.split('/')[-1]: 'OK' if key in keys2 else 'OPS',
             })
+
         df = pd.DataFrame(result)
         return df
 
@@ -145,6 +166,7 @@ class Application(tk.Tk):
     def update_view_tab(self):
         for tab in self.view_notebook.tabs():
             self.view_notebook.forget(tab)
+
         if self.base_file_path:
             base_tab = ttk.Frame(self.view_notebook)
             self.view_notebook.add(base_tab, text=f'Base: {self.base_file_path.split("/")[-1]}')
@@ -156,6 +178,7 @@ class Application(tk.Tk):
                 base_text.config(state='disabled')
                 line_count = content.count('\n') + 1
                 base_text.insert(tk.END, f"\n\n--- Total de linhas: {line_count} ---")
+
         for path in self.trans_file_paths:
             trans_tab = ttk.Frame(self.view_notebook)
             self.view_notebook.add(trans_tab, text=path.split('/')[-1])
@@ -169,71 +192,109 @@ class Application(tk.Tk):
                 trans_text.insert(tk.END, f"\n\n--- Total de linhas: {line_count} ---")
 
     def set_trans_file_langs(self):
+        # Cria uma nova janela
         lang_window = tk.Toplevel(self)
         lang_window.title("Configurações")
+
+        # Cria um Frame para o arquivo base
         base_frame = tk.Frame(lang_window)
         base_frame.pack(fill='x')
+
+        # Adiciona um Label para o nome do arquivo base
         base_label = tk.Label(base_frame, text=self.base_file_path)
         base_label.pack(side='left')
+
+        # Adiciona um OptionMenu para selecionar o idioma do arquivo base
         base_var = tk.StringVar()
         base_var.set(
-            self.trans_file_langs.get(self.base_file_path, "")) 
+            self.trans_file_langs.get(self.base_file_path, ""))  # Define o valor inicial para o idioma atual, se houver
         base_option_menu = tk.OptionMenu(base_frame, base_var, "pt", "en",
-                                         "es")  
+                                         "es")  # Adicione mais opções conforme necessário
         base_option_menu.pack(side='left')
+
+        # Salva a seleção sempre que ela é alterada
         base_var.trace("w",
                        lambda name, index, mode, p=self.base_file_path, v=base_var: self.save_trans_file_lang(p, v))
 
+        # Cria um Frame para cada arquivo de tradução
         for i, path in enumerate(self.trans_file_paths):
             frame = tk.Frame(lang_window)
             frame.pack(fill='x')
+
+            # Adiciona um Label para o nome do arquivo
             label = tk.Label(frame, text=path)
             label.pack(side='left')
+
+            # Adiciona um OptionMenu para selecionar o idioma
             var = tk.StringVar()
-            var.set(self.trans_file_langs.get(path, ""))  
-            option_menu = tk.OptionMenu(frame, var, "pt", "en", "es")  
+            var.set(self.trans_file_langs.get(path, ""))  # Define o valor inicial para o idioma atual, se houver
+            option_menu = tk.OptionMenu(frame, var, "pt", "en", "es")  # Adicione mais opções conforme necessário
             option_menu.pack(side='left')
+
+            # Salva a seleção sempre que ela é alterada
             var.trace("w", lambda name, index, mode, p=path, v=var: self.save_trans_file_lang(p, v))
+
     def save_trans_file_lang(self, path, var):
         self.trans_file_langs[path] = var.get()
         print(f"Idioma do arquivo {path} definido como {var.get()}")
+
     def analyze_files(self):
         only_missing = self.show_only_divergent.get()
         ignore_dicts = not self.show_indirect_keys.get()
+
         if not self.base_file_path or not self.trans_file_paths:
             print("Por favor, selecione o arquivo base e os arquivos de tradução antes de analisar.")
             return
+
+        # Limpa o Treeview existente, se houver
         for widget in self.analysis_results_tab.winfo_children():
             widget.destroy()
+
+        # Cria um novo Treeview dentro de um Frame com Scrollbars
         tree_frame = tk.Frame(self.analysis_results_tab)
         tree_frame.pack(fill='both', expand=True)
+
         tree_scrollbar_y = tk.Scrollbar(tree_frame)
         tree_scrollbar_y.pack(side='right', fill='y')
+
         tree_scrollbar_x = tk.Scrollbar(tree_frame, orient='horizontal')
         tree_scrollbar_x.pack(side='bottom', fill='x')
+
+
+        # Limpa as colunas existentes e adiciona as novas
         self.columns = ['ID', 'Chave', 'Valor']
         self.columns.extend([path.split('/')[-1].replace('.json', '').upper() for path in self.trans_file_paths])
+
         self.analysis_results_tree = ttk.Treeview(tree_frame, columns=self.columns, show='headings',
                                                   yscrollcommand=tree_scrollbar_y.set,
                                                   xscrollcommand=tree_scrollbar_x.set)
         self.analysis_results_tree.pack(expand=1, fill='both')
+
         tree_scrollbar_y.config(command=self.analysis_results_tree.yview)
         tree_scrollbar_x.config(command=self.analysis_results_tree.xview)
+        #self.analysis_results_tree.bind("<Button-3>", self.show_context_menu)
+
+        # Configura as colunas
         self.analysis_results_tree.column('ID', width=110, anchor='center')
         self.analysis_results_tree.column('Chave', width=200, anchor='w')
         self.analysis_results_tree.column('Valor', width=200, anchor='w')
-        for column_name in self.columns[3:]: 
+        for column_name in self.columns[3:]:  # Configura as colunas para cada arquivo de tradução
             self.analysis_results_tree.column(column_name, width=100, anchor='center')
+
+        # Configura os cabeçalhos
         self.analysis_results_tree.heading('ID', text='ID')
         self.analysis_results_tree.heading('Chave', text='Chave')
         self.analysis_results_tree.heading('Valor', text='Valor')
-        for column_name in self.columns[3:]:  
+        for column_name in self.columns[3:]:  # Configura os cabeçalhos para cada arquivo de tradução
             self.analysis_results_tree.heading(column_name, text=column_name)
+
         total_keys = 0
         missing_keys = {path.split('/')[-1].replace('.json', '').upper(): 0 for path in self.trans_file_paths}
+
         with open(self.base_file_path, 'r', encoding='utf-8') as f:
             base_data = json.load(f)
         base_keys = self.get_keys(base_data)
+
         for index, key in enumerate(base_keys):
             value = self.get_value(base_data, key)
             if isinstance(value, dict):
@@ -245,6 +306,7 @@ class Application(tk.Tk):
             missing = False
             for path in self.trans_file_paths:
                 column_name = path.split('/')[-1].replace('.json', '').upper()
+
                 with open(path, 'r', encoding='utf-8') as f:
                     trans_data = json.load(f)
                 if key in self.get_keys(trans_data):
@@ -257,16 +319,23 @@ class Application(tk.Tk):
                 self.analysis_results_tree.insert('', 'end', values=values)
 
         total_keys = len(base_keys)
+
+        # Adiciona um rodapé com a quantidade de chaves e chaves faltantes
         footer_frame = tk.Frame(self.analysis_results_tab)
         footer_frame.pack(fill='x')
+
         total_keys_label = tk.Label(footer_frame, text=f"Total de chaves: {total_keys}")
         total_keys_label.pack(side='left')
+
         for path, count in missing_keys.items():
             missing_keys_label = tk.Label(footer_frame, text=f"Chaves faltantes em {path}: {count}")
             missing_keys_label.pack(side='left')
+
+        # Adiciona um botão para adicionar a chave faltante
         add_key_button = tk.Button(self.analysis_results_tab, text="Adicionar chave faltante",
                                    command=self.add_missing_key)
         add_key_button.pack()
+        #self.analysis_results_tree.bind('<Button-3>', self.copy_to_clipboard)
 
     def analyze(self):
         self.analyze_files(only_missing=False)
@@ -291,8 +360,10 @@ class Application(tk.Tk):
         if not self.base_file_path or not self.trans_file_paths:
             tk.messagebox.showinfo("Aviso", "Por favor, selecione o arquivo base e pelo menos um arquivo de tradução.")
             return
+
         with open(self.base_file_path, 'r', encoding='utf-8') as f:
             base_data = json.load(f)
+
         def translate_value(value, from_lang, to_lang):
             translator = Translator()
             if isinstance(value, str):
@@ -305,15 +376,19 @@ class Application(tk.Tk):
         for trans_file_path in self.trans_file_paths:
             with open(trans_file_path, 'r', encoding='utf-8') as f:
                 trans_data = json.load(f)
+
             from_lang = self.trans_file_langs.get(self.base_file_path, "pt")
             to_lang = self.trans_file_langs.get(trans_file_path, "en")
+
             for key in self.get_keys(base_data):
                 if key not in self.get_keys(trans_data):
                     value = self.get_value(base_data, key)
                     translated_value = translate_value(value, from_lang, to_lang)
                     self.add_key(trans_data, key, translated_value)
+
             with open(trans_file_path, 'w', encoding='utf-8') as f:
                 json.dump(trans_data, f, ensure_ascii=False, indent=4)
+
         tk.messagebox.showinfo("Informação", "A adição das chaves e seus valores traduzidos foi concluída.")
         self.analyze_files()
 
@@ -324,36 +399,56 @@ class Application(tk.Tk):
         self.context_menu.add_command(label="Traduzir para espanhol", command=self.translate_to_spanish)
 
     def show_context_menu(self, event):
+        # Identifica a linha sob o cursor
         row_id = self.analysis_results_tree.identify_row(event.y)
         column_id = self.analysis_results_tree.identify_column(event.x)
+
+        # Se nenhuma linha foi identificada, não faz nada
         if row_id == "":
             return
+
+        # Seleciona a linha sob o cursor
         self.analysis_results_tree.selection_set(row_id)
+
+        # Armazena a linha e coluna selecionada
         self.selected_row = row_id
         self.selected_col = column_id
+
+        # Mostra o menu de contexto
         try:
             self.context_menu.tk_popup(event.x_root, event.y_root)
         finally:
             self.context_menu.grab_release()
 
     def copy_to_clipboard(self):
+        # Obtém a linha e coluna selecionada
         row_id = self.selected_row
         column_id = self.selected_col
+
+        # Obtém o valor da célula
         cell_value = self.analysis_results_tree.set(row_id, column_id)
+
+        # Copia o valor para a área de transferência
         self.clipboard_clear()
         self.clipboard_append(cell_value)
+
+        # Cria uma janela de diálogo para mostrar a mensagem
         dialog = tk.Toplevel(self)
         dialog.title("Copiado para a área de transferência")
         tk.Label(dialog, text=f"Valor copiado: {cell_value}").pack()
+
+        # Centraliza a janela de diálogo na parte superior da tela
         window_width = dialog.winfo_reqwidth()
         window_height = dialog.winfo_reqheight()
         position_right = int(dialog.winfo_screenwidth() / 2 - window_width / 2)
         position_down = int(dialog.winfo_screenheight() / 3 - window_height / 2)
         dialog.geometry("+{}+{}".format(position_right, position_down))
+
+        # Fecha a janela de diálogo após 2 segundos
         def close_dialog():
             dialog.destroy()
 
-        threading.Timer(4, close_dialog).start()
+        threading.Timer(2, close_dialog).start()
 
     @staticmethod
     def to_english(text, from_lang):
@@ -368,26 +463,110 @@ class Application(tk.Tk):
         return translation
 
     def translate_to_english(self):
+        # Verifica se uma linha está selecionada
         selected_items = self.analysis_results_tree.selection()
         if not selected_items:
             tk.messagebox.showinfo("Aviso", "Por favor, selecione uma linha para traduzir.")
             return
+
+        # Obtém o idioma do arquivo de tradução
         from_lang = self.trans_file_langs.get(self.base_file_path, "pt")
+
+        # Obtém a linha selecionada
         selected_item = selected_items[0]
+
+        # Obtém o valor da célula na coluna "Valor"
         cell_value = self.analysis_results_tree.set(selected_item, '#3')
+
+        # Traduz o valor para o inglês
         translated_value = self.to_english(cell_value, from_lang)
+
+        # Mostra o valor em um alerta
         tk.messagebox.showinfo("Tradução para inglês", f"Valor da chave: {translated_value}")
 
     def translate_to_spanish(self):
+        # Verifica se uma linha está selecionada
         selected_items = self.analysis_results_tree.selection()
         if not selected_items:
             tk.messagebox.showinfo("Aviso", "Por favor, selecione uma linha para traduzir.")
             return
+
+        # Obtém o idioma do arquivo de tradução
         from_lang = self.trans_file_langs.get(self.base_file_path, "pt")
+
+        # Obtém a linha selecionada
         selected_item = selected_items[0]
+
+        # Obtém o valor da célula na coluna "Valor"
         cell_value = self.analysis_results_tree.set(selected_item, '#3')
+
+        # Traduz o valor para o espanhol
         translated_value = self.to_spanish(cell_value, from_lang)
+
+        # Mostra o valor em um alerta
         tk.messagebox.showinfo("Tradução para espanhol", f"Valor da chave: {translated_value}")
+
+    def check_duplicate_keys_in_same_level(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as f:
+            content = f.read()
+
+        duplicates = {}
+        stack = []
+        keys_seen_at_current_level = set()
+        lines = content.split('\n')
+        for line in lines:
+            stripped = line.strip()
+            if '{' in stripped:
+                stack.append(stripped.split(':')[0].strip().strip('"'))
+                keys_seen_at_current_level = set()
+            elif '}' in stripped and stack:
+                stack.pop()
+            elif ':' in stripped:
+                key = stripped.split(':')[0].strip().strip('"')
+                path = '.'.join(stack + [key])
+                if key in keys_seen_at_current_level:
+                    if path in duplicates:
+                        duplicates[path] += 1
+                    else:
+                        duplicates[path] = 2
+                else:
+                    keys_seen_at_current_level.add(key)
+        return duplicates
+
+    def find_duplicates(self, data, path=''):
+        duplicates = []
+        keys = list(data.keys())
+        for i in range(len(keys)):
+            for j in range(i + 1, len(keys)):
+                if keys[i] == keys[j]:
+                    duplicates.append(f"{path}.{keys[i]}" if path else keys[i])
+        for key, value in data.items():
+            new_path = f"{path}.{key}" if path else key
+            if isinstance(value, dict):
+                duplicates.extend(self.find_duplicates(value, new_path))
+        return duplicates
+
+    def check_duplicates(self):
+        # Solicita ao usuário que selecione um arquivo
+        file_path = filedialog.askopenfilename()
+
+        # Se nenhum arquivo foi selecionado, retorna
+        if not file_path:
+            tk.messagebox.showinfo("Aviso", "Por favor, selecione um arquivo.")
+            return
+
+        # Verifica se há duplicatas no arquivo
+        duplicates = self.check_duplicate_keys_in_same_level(file_path)
+        if duplicates:
+            # Limpa o Treeview existente
+            for i in self.duplicates_results_tree.get_children():
+                self.duplicates_results_tree.delete(i)
+
+            # Adiciona as duplicatas ao Treeview
+            for duplicate, count in duplicates.items():
+                self.duplicates_results_tree.insert('', 'end', text=f"{duplicate} {count}x")
+        else:
+            tk.messagebox.showinfo("Nenhuma duplicata encontrada", "Nenhuma chave ou nó duplicado foi encontrado.")
 
 if __name__ == "__main__":
     app = Application()
